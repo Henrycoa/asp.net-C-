da65a964c94163752da43b896230dadd5bb901b8
Henry Buena <henrycoa123@gmail.com>
2025-11-23 01:57:29 +0800

    Railway-ready stable backend with MySQL

--- obj/Release/net9.0/backend.AssemblyInfo.cs
+++ obj/Release/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+627ea2e36343e9d726661a70427f563ced1e35b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25757584ffefd4f6ba94a97858d35bcb7bf3ee99")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
